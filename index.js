802e1d9308c40b81d201c7129a1ff4259a677d8b
noaheakin <noaheakin@gmail.com>
2021-02-18 14:45:59 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function findMinAndRemoveSorted(arr) {
+    return arr.shift()
+}
 
+function merge(arr1, arr2) {
+    let finalArray = []
+    while (arr1.length > 0 && arr2.length > 0) {
+        if (arr1[0] < arr2[0]) {
+            finalArray.push(findMinAndRemoveSorted(arr1))
+        } else {
+            finalArray.push(findMinAndRemoveSorted(arr2))
+        }
+    }
+    return finalArray.concat(arr1).concat(arr2)
+}
+
+function mergeSort(arr) {
+    let midpoint = arr.length / 2;
+    let firstHalf = arr.slice(0, midpoint)
+    let secondHalf = arr.slice(midpoint)
+    let sorted
+    if (arr.length < 2) {
+        return arr
+    } else {
+        sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+    return sorted
+}
